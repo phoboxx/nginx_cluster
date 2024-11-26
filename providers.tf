59695ac2a78685fbf47c74d72e16b582246823f7
Frederic Legault <legault.f123@gmail.com>
2024-11-26 11:58:11 -0500

    remove the provider configuration

--- providers.tf
+++ providers.tf
@@ -1,13 +1,8 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
-
-# # Configure the AWS Provider
-# provider "aws" {
-#   region = "us-east-2"
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
