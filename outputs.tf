48b14e886a04a44dea659f3577918a8546de734a
Frederic Legault <legault.f123@gmail.com>
2024-11-26 12:09:29 -0500

    change provider

--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,3 @@ output "ssh" {
   value = "ssh -i [SSH_KEY_FILE] ec2-user@${aws_instance.nginx.public_dns}"
 }
 
-output "test_output_version_1_0_1" {
-  value = "This is a test for version 1.0.1"
-}
